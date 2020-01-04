560eb9adc94bf8b4a99d937fd9bc175709423428
Heath Gargit <heath@gargit.games>
2020-01-04 15:30:49 +1100

    Repo Init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "¡Hola, amigos!"
+    end
+
+    def laterinos
+        render html: "Later, m8!"
+    end
+end
